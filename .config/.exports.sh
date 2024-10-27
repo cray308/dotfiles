4d1104f7c9823ed5639e994b0aa8aa44075af12c
Chris Ray <cpray504@gmail.com>
2024-10-27 18:59:58 -0400

    Remove unneeded files

--- .config/.exports.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/opt/android-sdk/cmdline-tools/latest/bin
-
--- .profile
+++
@@ -1,4 +0,0 @@
-source ~/.xinitrc
-export EDITOR='vim'
-export PAGER='less'
-export BROWSER='chromium'
